583db4ff3954f5513f5d983a0005f73344563b5c
Francisco Solis <30329003+Im-Fran@users.noreply.github.com>
2026-07-28 16:30:33 -0400

    feat: configurar react-router-dom con rutas / , /display y 404

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import LandingPage from '@/pages/landing'
+import DisplayPage from '@/pages/display'
+import NotFoundPage from '@/pages/not-found'
 
 function App() {
-  return <LandingPage />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/display" element={<DisplayPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
